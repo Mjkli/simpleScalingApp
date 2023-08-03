318b881e993992020ad0fcbf60506e785af45f99
Mjkli <Mjkli@users.noreply.github.com>
2023-08-03 11:58:23 -0700

    adding internet gateway

--- infrastructure/tf/network.tf
+++ infrastructure/tf/network.tf
@@ -8,6 +8,12 @@ resource "aws_vpc" "main-vpc" {
     }
 }
 
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main-vpc.id
+}
+
+
+
 resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.main-vpc.id
     cidr_block = "10.0.0.0/24"
