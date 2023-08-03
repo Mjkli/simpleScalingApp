5b4a7697e5cfae58c25ec762c7fdf74ef0fb8651
Mjkli <Mjkli@users.noreply.github.com>
2023-08-03 11:40:51 -0700

    adding 2 different subnets

--- infrastructure/tf/network.tf
+++ infrastructure/tf/network.tf
@@ -11,9 +11,21 @@ resource "aws_vpc" "main-vpc" {
 resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.main-vpc.id
     cidr_block = "10.0.0.0/24"
+    availability_zone = "us-west-1a"
 
     tags = {
         Name = "subnet-1"
+        project = "simpleScalingApp"
+    }
+}
+
+resource "aws_subnet" "subnet-2" {
+    vpc_id = aws_vpc.main-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-west-1b"
+
+    tags = {
+        Name = "subnet-2"
         project = "simpleScalingApp"
     }
 }
